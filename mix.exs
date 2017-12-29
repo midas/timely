b205b2b58e89fc1986d8cbadbe726aba498ed6e9
Jason Harrelson <jason@lookforwardenterprises.com>
2017-12-29 15:13:24 -0600

    Add Hex package metadata.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,33 @@ defmodule Timely.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
+  end
+
+  defp description do
+    """
+    Provides functions for converting, comparing and shifting Elixir dates/times.
+    """
+  end
+
+  defp package do
+    [
+      name: :timely,
+      files: [
+        "lib",
+        "mix.exs",
+        "README*",
+        "LICENSE*"
+      ],
+      maintainers: ["C. Jason Harrelson"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/midas/timely",
+        "Docs"   => "https://hexdocs.pm/timely/0.1.0"
+      }
+    ]
   end
 
   def application do
